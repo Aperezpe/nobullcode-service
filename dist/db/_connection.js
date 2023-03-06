5ace8484ea06a73c1449e4d9006d760a456d9eaa
Abraham Perez - a0p0cdd <Abraham.Perez0@walmart.com>
2023-03-06 08:55:19 -0600

    adds build with tsc in github actions

--- dist/db/_connection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var knexfile_1 = __importDefault(require("./knexfile"));
-var knex_1 = __importDefault(require("knex"));
-exports.default = (0, knex_1.default)(knexfile_1.default[process.env.NODE_ENV || 'development']);
